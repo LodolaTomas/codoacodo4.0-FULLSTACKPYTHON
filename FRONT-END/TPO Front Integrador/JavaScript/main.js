3cfe29e4c5011772e7b49c4782173b1a44602a98
LodolaTomas <tomaslodola1@gmail.com>
2021-10-05 00:02:18 -0300

    m caigo con js

--- FRONT-END/TPO Front Integrador/JavaScript/main.js
+++ FRONT-END/TPO Front Integrador/JavaScript/main.js
@@ -163,3 +163,18 @@ const styles = {
     },
   ],
 };
+
+
+/* SCROLL DOWN  */
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
